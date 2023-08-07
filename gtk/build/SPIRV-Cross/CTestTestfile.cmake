aef29e7cdb6252ae307b3b8555173e9767a7bb25
dustingibson <dustingibson@outlook.com>
2023-08-06 20:35:12 -0500

    Oops. Forgot gitignore.

--- gtk/build/SPIRV-Cross/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dustin/code/snes9x/external/SPIRV-Cross
-# Build directory: /home/dustin/code/snes9x/gtk/build/SPIRV-Cross
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
